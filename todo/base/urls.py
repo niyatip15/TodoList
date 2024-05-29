45a259ec580af67a31561c3e4a2886900e1de4b9
niyatip15 <niyupathak1503@gmail.com>
2024-05-29 15:00:31 +0530

    user registration functionality.

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
    path('login/',CustomLoginView.as_view(),name='login'),
    path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
+   path('register/',Registeration.as_view(),name='register'),
    path('',TasksList.as_view(),name='tasks'),
    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
    path('task-create/',TaskCreate.as_view(),name='task-create'),
